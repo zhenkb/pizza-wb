e52b4c446f8cc7a80e45e38d86a1ff55b8ac20f6
zhenkb <110013842+zhenkb@users.noreply.github.com>
2024-12-25 00:42:32 +0800

    Update nav.php

--- nav.php
+++ nav.php
@@ -4,7 +4,7 @@
     <div class="w3-sidebar w3-bar-block w3-card w3-animate-left" style="display:none" id="mySidebar">
         <button class="w3-bar-item w3-button w3-large" onclick="w3_close()">Close &times;</button>
         <a href="home.php" class="w3-bar-item w3-button">首頁</a>
-        <a href="#" class="w3-bar-item w3-button">Link 2</a>
+        <a href="login.php" class="w3-bar-item w3-button">Link 2</a>
         <a href="#" class="w3-bar-item w3-button">Link 3</a>
     </div>
     ling;
