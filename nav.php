38bd0bd8d5d3146fb974a8788db7dce8146757a9
zhenkb <110013842+zhenkb@users.noreply.github.com>
2024-12-24 23:11:34 +0800

    Create nav.php

---
+++ nav.php
@@ -0,0 +1,19 @@
+<?php
+    $ling=<<<ling
+    <!-- Sidebar -->
+    <div class="w3-sidebar w3-bar-block w3-card w3-animate-left" style="display:none" id="mySidebar">
+        <button class="w3-bar-item w3-button w3-large" onclick="w3_close()">Close &times;</button>
+        <a href="home.php" class="w3-bar-item w3-button">首頁</a>
+        <a href="#" class="w3-bar-item w3-button">Link 2</a>
+        <a href="#" class="w3-bar-item w3-button">Link 3</a>
+    </div>
+    ling;
+    
+    $link=<<<link
+    <div class="w3-bar w3-light-blue">
+        <a href="#" class="w3-bar-item w3-button">首頁</a>
+        <a href="#" class="w3-bar-item w3-button">Link 1</a>
+        <a href="#" class="w3-bar-item w3-button">Link 2</a>
+        <a href="#" class="w3-bar-item w3-button">Link 3</a>
+    </div>
+    link;
