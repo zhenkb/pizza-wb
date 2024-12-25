699dc6b3b30bd2726dcf9174e75a092207247afd
zhenkb <110013842+zhenkb@users.noreply.github.com>
2024-12-26 00:47:52 +0800

    Create pizza4.php

---
+++ pizza4.php
@@ -0,0 +1,67 @@
+<?php
+    include("nav.php");
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <?=$ling?>
+
+    <div id="main">
+        <div class="w3-deep-purple">
+            <button id="openNav" class="w3-button w3-deep-purple w3-xlarge" onclick="w3_open()">&#9776;</button>
+            <div class="w3-container">
+                <h1>小饞貓貪吃店</h1>
+            </div>
+            <?=$link?>
+        </div>
+    <div class="w3-container w3-center">
+    <h2>麵包超人披薩</h2>
+
+    <div class="w3-card-4" style="display: flex; justify-content: center; width: 50%; margin: 0 auto;">
+        <!-- Image Section -->
+        <img src="pizza/Pizza-4.jpg" alt="Alps" style="width: 50%;">
+
+        <!-- Text Section -->
+        <div style="flex: 1; padding-left: 20px;">
+        <h3 style="text-align: left;">
+                麵包超人披薩是一款為孩子和卡通愛好者設計的披薩，
+                它的外觀以可愛的麵包超人角色為靈感，形狀獨特，色彩繽紛。
+                披薩的餅皮酥脆，搭配濃郁的番茄醬、香濃的莫札瑞拉起司，並以各種蔬菜和香腸等配料做裝飾。
+                這款披薩不僅味道美味，還非常適合家庭聚會，尤其是小朋友的最愛，讓用餐變得更加有趣。
+            </h3>
+        </div>
+    </div>
+</div>
+
+
+        <!-- Buttons Section -->
+        <div style="text-align: center; margin-top: 20px;">
+
+            <a href="index.php" class="w3-button w3-green">加入購物車</a>
+            <a href="order.php" class="w3-button w3-blue">查看訂單</a>
+        </div>
+        </div>
+    </div>
+    <script>
+        function w3_open() {
+        document.getElementById("main").style.marginLeft = "25%";
+        document.getElementById("mySidebar").style.width = "25%";
+        document.getElementById("mySidebar").style.display = "block";
+        document.getElementById("openNav").style.display = 'none';
+        }
+        function w3_close() {
+        document.getElementById("main").style.marginLeft = "0%";
+        document.getElementById("mySidebar").style.display = "none";
+        document.getElementById("openNav").style.display = "inline-block";
+        }
+    </script>
+</body>
+</html>
