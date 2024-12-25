10d013d97a12af18ee7aed473b9078e9550c0c65
zhenkb <110013842+zhenkb@users.noreply.github.com>
2024-12-26 00:48:01 +0800

    Create pizza7.php

---
+++ pizza7.php
@@ -0,0 +1,67 @@
+<?php
+    include("nav.php");
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <?=$ling?>
+
+    <div id="main">
+        <div class="w3-deep-purple">
+            <button id="openNav" class="w3-button w3-deep-purple w3-xlarge" onclick="w3_open()">&#9776;</button>
+            <div class="w3-container">
+                <h1>小饞貓貪吃店</h1>
+            </div>
+            <?=$link?>
+        </div>
+    <div class="w3-container w3-center">
+    <h2>聖誕老人披薩</h2>
+
+    <div class="w3-card-4" style="display: flex; justify-content: center; width: 50%; margin: 0 auto;">
+        <!-- Image Section -->
+        <img src="pizza/Pizza-7.jpg" alt="Alps" style="width: 50%;">
+
+        <!-- Text Section -->
+        <div style="flex: 1; padding-left: 20px;">
+        <h4 style="text-align: left;">
+                聖誕老人披薩是一款充滿節日氛圍的披薩，將傳統的聖誕節食材與披薩完美結合。
+                這款披薩的餅皮酥脆，搭配了濃郁的番茄醬和融化的起司，並且添加了火雞肉、蔓越莓醬、烤蔬菜等，帶來溫馨的聖誕味道。
+                火雞肉的鮮美與蔓越莓的酸甜相得益彰，增添了獨特的節日風味，讓每一口都充滿節日的歡樂。
+                聖誕老人披薩是聖誕節家庭聚會或派對的完美選擇，讓每一位食客都感受到聖誕的溫暖與快樂。
+            </h4>
+        </div>
+    </div>
+</div>
+
+
+        <!-- Buttons Section -->
+        <div style="text-align: center; margin-top: 20px;">
+
+            <a href="index.php" class="w3-button w3-green">加入購物車</a>
+            <a href="order.php" class="w3-button w3-blue">查看訂單</a>
+        </div>
+        </div>
+    </div>
+    <script>
+        function w3_open() {
+        document.getElementById("main").style.marginLeft = "25%";
+        document.getElementById("mySidebar").style.width = "25%";
+        document.getElementById("mySidebar").style.display = "block";
+        document.getElementById("openNav").style.display = 'none';
+        }
+        function w3_close() {
+        document.getElementById("main").style.marginLeft = "0%";
+        document.getElementById("mySidebar").style.display = "none";
+        document.getElementById("openNav").style.display = "inline-block";
+        }
+    </script>
+</body>
+</html>
