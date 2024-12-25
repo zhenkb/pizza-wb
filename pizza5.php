8150b8a47d768e1f868869645e202db7ce1cbf4f
zhenkb <110013842+zhenkb@users.noreply.github.com>
2024-12-26 00:48:06 +0800

    Create pizza5.php

---
+++ pizza5.php
@@ -0,0 +1,67 @@
+<?php
+    include("nav.php");
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <?=$ling?>
+
+    <div id="main">
+        <div class="w3-deep-purple">
+            <button id="openNav" class="w3-button w3-deep-purple w3-xlarge" onclick="w3_open()">&#9776;</button>
+            <div class="w3-container">
+                <h1>小饞貓貪吃店</h1>
+            </div>
+            <?=$link?>
+        </div>
+    <div class="w3-container w3-center">
+    <h2>起司披薩</h2>
+
+    <div class="w3-card-4" style="display: flex; justify-content: center; width: 50%; margin: 0 auto;">
+        <!-- Image Section -->
+        <img src="pizza/Pizza-5.jpg" alt="Alps" style="width: 50%;">
+
+        <!-- Text Section -->
+        <div style="flex: 1; padding-left: 20px;">
+        <h3 style="text-align: left;">
+                起司披薩是一款簡單而經典的披薩，主角是濃郁的起司。
+                這款披薩的餅皮酥脆，搭配了滿滿的莫札瑞拉起司，每一口都能享受到絲滑的口感。
+                起司披薩的美味來自於它的純粹，無需過多的配料，讓起司的香濃味道成為焦點。
+                是許多人最愛的選擇，特別適合喜歡奶香味的朋友，無論是單獨食用還是搭配其他菜肴，都是不敗的美味。
+            </h3>
+        </div>
+    </div>
+</div>
+
+
+        <!-- Buttons Section -->
+        <div style="text-align: center; margin-top: 20px;">
+
+            <a href="index.php" class="w3-button w3-green">加入購物車</a>
+            <a href="order.php" class="w3-button w3-blue">查看訂單</a>
+        </div>
+        </div>
+    </div>
+    <script>
+        function w3_open() {
+        document.getElementById("main").style.marginLeft = "25%";
+        document.getElementById("mySidebar").style.width = "25%";
+        document.getElementById("mySidebar").style.display = "block";
+        document.getElementById("openNav").style.display = 'none';
+        }
+        function w3_close() {
+        document.getElementById("main").style.marginLeft = "0%";
+        document.getElementById("mySidebar").style.display = "none";
+        document.getElementById("openNav").style.display = "inline-block";
+        }
+    </script>
+</body>
+</html>
