200df6341d8422f618e93e4d1194a7de0d04800b
zhenkb <110013842+zhenkb@users.noreply.github.com>
2024-12-26 01:03:10 +0800

    Create pizza20.php

---
+++ pizza20.php
@@ -0,0 +1,65 @@
+<?php
+    include("nav.php");
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <?=$ling?>
+
+    <div id="main">
+        <div class="w3-deep-purple">
+            <button id="openNav" class="w3-button w3-deep-purple w3-xlarge" onclick="w3_open()">&#9776;</button>
+            <div class="w3-container">
+                <h1>小饞貓貪吃店</h1>
+            </div>
+            <?=$link?>
+        </div>
+    <div class="w3-container w3-center">
+    <h2>立體喵咪披薩</h2>
+
+    <div class="w3-card-4" style="display: flex; justify-content: center; width: 50%; margin: 0 auto;">
+        <!-- Image Section -->
+        <img src="pizza/Pizza-20.jpg" alt="Alps" style="width: 50%;">
+
+        <!-- Text Section -->
+        <div style="flex: 1; padding-left: 20px;">
+        <h4 style="text-align: left;">
+                立體喵咪披薩是一款以可愛的喵咪為靈感的創意披薩，將可愛的造型與美味結合。這款披薩的外觀像是一隻小巧的貓咪，配料如黑橄欖、黃椒和番茄等，用來打造貓咪的眼睛和耳朵，讓人不禁對它產生喜愛。披薩的基底由脆薄的餅皮製成，搭配番茄醬、融化的莫札瑞拉起司，味道濃郁又滑順。
+                除了可愛的外觀，這款披薩的內餡還有新鮮的蔬菜與香腸，搭配濃厚的起司，使其成為一款視覺與味覺兼具的美味披薩。無論是家庭聚會還是朋友聚餐，立體喵咪披薩都是一道充滿樂趣的美食選擇。
+            </h4>
+        </div>
+    </div>
+</div>
+
+
+        <!-- Buttons Section -->
+        <div style="text-align: center; margin-top: 20px;">
+
+            <a href="index.php" class="w3-button w3-green">加入購物車</a>
+            <a href="order.php" class="w3-button w3-blue">查看訂單</a>
+        </div>
+        </div>
+    </div>
+    <script>
+        function w3_open() {
+        document.getElementById("main").style.marginLeft = "25%";
+        document.getElementById("mySidebar").style.width = "25%";
+        document.getElementById("mySidebar").style.display = "block";
+        document.getElementById("openNav").style.display = 'none';
+        }
+        function w3_close() {
+        document.getElementById("main").style.marginLeft = "0%";
+        document.getElementById("mySidebar").style.display = "none";
+        document.getElementById("openNav").style.display = "inline-block";
+        }
+    </script>
+</body>
+</html>
